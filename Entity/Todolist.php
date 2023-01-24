99781186501e0c42d477966920e62e70def2aab8
RizalFIrdaus <rizal.firdaus117@gmail.com>
2023-01-24 08:19:02 +0700

    Todolist Entity setter and getter

--- Entity/Todolist.php
+++ Entity/Todolist.php
@@ -2,6 +2,21 @@
 
 namespace Entity;
 
-class wad
+class TodoList
 {
+    private string $todolist;
+
+    public function __construct(string $todolist)
+    {
+        $this->todolist = $todolist;
+    }
+
+    public function getTodo(): string
+    {
+        return $this->todolist;
+    }
+    public function setTodo(string $todolist): void
+    {
+        $this->todolist = $todolist;
+    }
 }
